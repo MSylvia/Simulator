5cf0dda47396658c3434f3631617b900ce8d782f
Tim Specht <dev@tim-specht.com>
2017-01-03 08:58:02 +0100

    State before refactoring

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
 import {Actor} from './src/';
+
+class Bird extends Actor{
+    constructor(){
+        super();
+        // Set Image Src
+        super.setImageSrc('path/to/image.png');
+        
+    }
+}
